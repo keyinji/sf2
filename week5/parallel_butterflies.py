facff355e8b6dc4ce348c096d0af1870fc9e1126
Ke Yin Ji <ke.ji@dawsoncollege.qc.ca>
2025-02-27 09:31:51 -0500

    Ok

--- week5/parallel_butterflies.py
+++ week5/parallel_butterflies.py
@@ -1,18 +1,33 @@
-def new(kinds, counts, sighting):
-    if sighting not in kinds:
-        kinds.append(sighting)
-        counts.append(0)
-    i = kinds.index(sighting)
-    counts[i] += 1
+# def new(kinds, counts, sighting):
+#     if sighting not in kinds:
+#         kinds.append(sighting)
+#         counts.append(0)
+#     i = kinds.index(sighting)
+#     counts[i] += 1
 
-kinds = ['Monarch', 'Painted Lady', "Bronze Copper", "Orange"]
-counts = [5, 2, 12, 7]
+# kinds = ['Monarch', 'Painted Lady', "Bronze Copper", "Orange"]
+# counts = [5, 2, 12, 7]
 
-for i in range(len(kinds)):
-    print(f'{kinds[i]}: {counts[i]}')
+# for i in range(len(kinds)):
+#     print(f'{kinds[i]}: {counts[i]}')
 
-new(kinds, counts, 'Monarch')
+# new(kinds, counts, 'Monarch')
 
 
-for i in range(len(kinds)):
-    print(f'{kinds[i]}: {counts[i]}')
+# for i in range(len(kinds)):
+#     print(f'{kinds[i]}: {counts[i]}')
+
+butterflies = {"m": 5, "j":7}
+
+for k in butterflies:
+    print(f'{k}: {butterflies[k]}')
+
+def new_sighting(butterflies, sighting):
+    butterflies[sighting] = butterflies.get(sighting) + 1
+
+
+new_sighting(butterflies ,'m')
+
+for k in butterflies:
+    print(f'{k}: {butterflies[k]}')
+    
