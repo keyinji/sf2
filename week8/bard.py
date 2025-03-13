97b525ff2d37dc616432943e730766327a887f83
Ke Yin Ji <92763672+keyinji@users.noreply.github.com>
2025-03-13 09:42:12 -0400

    Create bard.py

---
+++ week8/bard.py
@@ -0,0 +1,48 @@
+'''
+Input specificatrion:
+first line is N number of vilalgerts
+second line E number of eveniong 
+next e lines contain lst lof villagers present each line beins with [ositive integer k
+the number of villagers present this evening followed by K posiytib itegers separated by
+spaces representing the villagers 
+note that the bard is villager 1
+
+sample input 
+4
+3
+2 1 2
+3 2 3 4
+3 4 2 1
+
+1
+2
+4
+
+8
+5
+4 1 3 5 4
+2 5 6
+3 6 7 8
+2 6 2
+5 2 6 8 1
+
+1
+2
+6
+8
+
+5 
+3
+2 1 3
+2 2 1
+...
+
+1
+'''
+
+villagers = int(input())
+evenings = int(input())
+for i in range(evenings):
+    no_of_v = int(input)
+    for i in range(no_of_v):
+        pass
